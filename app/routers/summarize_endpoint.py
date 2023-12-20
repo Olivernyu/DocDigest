e7c974df13a6eadd1448f5a26ff6abe861d72bab
Leo Li <leo4life@163.com>
2023-12-20 06:39:17 -0500

    fix tests

--- app/routers/summarize_endpoint.py
+++ app/routers/summarize_endpoint.py
@@ -8,24 +8,26 @@ from ..shared_resources import page_data_store
 router = APIRouter()
 
 @router.get("/{page_id}")
-async def summarize_page(page_id: str, ai_provider: str = Query("openai", description="AI provider for summarization")):
+async def summarize_page(
+        page_id: str,
+        ai_provider: str = Query(
+            "openai", description="AI provider for summarization")
+):
     """
     Summarize the page by providing the page_id and an optional AI provider
     """
-    try:
-        page_data = page_data_store.get(page_id)
-        if page_data is None:
-            raise HTTPException(status_code=404, detail="Page not found")
-        text_from_html = page_data.get("text_from_html")
-        if text_from_html is None:
-            raise Exception("Text from html is not in page_data.")
+    page_data = page_data_store.get(page_id)
+    if page_data is None:
+        raise HTTPException(status_code=404, detail="Page not found")
+    text_from_html = page_data.get("text_from_html")
+    if text_from_html is None:
+        raise Exception("Text from html is not in page_data.")
 
-        if ai_provider == "openai":
-            openai_service = OpenAIService()
-            summary = openai_service.summarize_text(text_from_html)
-        else:
-            raise HTTPException(status_code=400, detail="Unsupported AI provider")
+    if ai_provider == "openai":
+        openai_service = OpenAIService()
+        summary = openai_service.summarize_text(text_from_html)
+    else:
+        raise HTTPException(
+            status_code=400, detail="Unsupported AI provider")
 
-        return {"summary": summary}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    return {"summary": summary}
---
+++ test/routers/test_summarize.py
@@ -0,0 +1,31 @@
+from fastapi.testclient import TestClient
+from app.main import app
+from unittest.mock import patch, MagicMock
+
+from app.services.openai import OpenAIService
+
+client = TestClient(app)
+
+
+def test_summarize_page():
+    # Example data
+    test_data = {"text": "Sample page data", "url": "https://engineering.nyu.edu/"}
+
+    response = client.post("/savepage", json=test_data)
+    page_id = response.json()["id"]
+    mock_summary = "This is a mock summary."
+
+    with patch('app.services.openai.OpenAIService.__new__') as mock_new:
+        # Prevent creating actual instance and reading API Key
+        # Create a mock instance with a mocked summarize_text method
+        mock_instance = MagicMock()
+        mock_instance.summarize_text.return_value = mock_summary
+        mock_new.return_value = mock_instance
+
+        response = client.get(f"/summarize/{page_id}")
+
+        # Asserts
+        mock_new.assert_called_once()
+        mock_instance.summarize_text.assert_called_once()
+        assert response.status_code == 200
+        assert response.json() == {"summary": mock_summary}
